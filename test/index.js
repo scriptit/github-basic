5f130452dc692d18b4dedbd397b6c2acf6ac5186
Forbes Lindesay <forbes@lindesay.co.uk>
2014-03-28 10:07:51 +0000

    Fix rate limiting of tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,24 @@
 var assert = require('assert')
 var github = require('../')
 
+var options = {
+  auth: {
+    type: 'oauth',
+    token: '90993e4e47b0fdd1f51f4c67b17368c62a3d6097'
+  }
+}
+
+var hostOptions = {
+  host: 'github.com',
+  auth: {
+    type: 'oauth',
+    token: '90993e4e47b0fdd1f51f4c67b17368c62a3d6097'
+  }
+}
+
 it('can make simple api requests', function (done) {
   this.timeout(5000)
-  github.json('get', '/users/:user/gists', {user: 'ForbesLindesay'}, function (err, res) {
+  github.json('get', '/users/:user/gists', {user: 'ForbesLindesay'}, options, function (err, res) {
     if (err) return done(err)
     assert(res.statusCode === 200)
     assert(Array.isArray(res.body))
@@ -16,7 +31,7 @@ it('can make simple api requests', function (done) {
 })
 it('can have a `host` option passed in', function (done) {
   this.timeout(5000)
-  github.buffer('head', '/ForbesLindesay/github-basic', null, {host: 'github.com'}, function (err, res) {
+  github.buffer('head', '/ForbesLindesay/github-basic', null, hostOptions, function (err, res) {
     if (err) return done(err)
     assert(res.statusCode === 200)
     done()
@@ -27,7 +42,7 @@ it('can make streaming requests', function (done) {
   github.stream('/repos/:owner/:repo/subscribers', {
     owner: 'visionmedia',
     repo: 'jade'
-  }).buffer(function (err, res) {
+  }, options).buffer(function (err, res) {
     if (err) return done(err)
     assert(Array.isArray(res))
     assert(res.length > 150)
@@ -35,15 +50,14 @@ it('can make streaming requests', function (done) {
   })
 })
 it('can make streaming requests for commits', function (done) {
-  this.timeout(40000)
+  this.timeout(20000)
   github.stream('/repos/:owner/:repo/commits', {
-    owner: 'visionmedia',
-    repo: 'jade'
-  }).buffer(function (err, res) {
+    owner: 'ForbesLindesay',
+    repo: 'browserify-middleware'
+  }, options).buffer(function (err, res) {
     if (err) return done(err)
     assert(Array.isArray(res))
-    assert(res.length > 150)
-    console.dir(res)
+    assert(res.length > 140)
     done()
   })
 })
@@ -52,7 +66,7 @@ it('can make streaming requests with just one page', function (done) {
   github.stream('/repos/:owner/:repo/subscribers', {
     owner: 'ForbesLindesay',
     repo: 'github-basic'
-  }).buffer(function (err, res) {
+  }, options).buffer(function (err, res) {
     if (err) return done(err)
     assert(Array.isArray(res))
     done()

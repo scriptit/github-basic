2564f9bd284c281090989c4b45b877787a64df3f
Forbes Lindesay <forbes.lindesay@red-gate.com>
2014-05-09 14:12:17 +0100

    Update jsdoc comments

--- index.js
+++ index.js
@@ -68,8 +68,6 @@ exports.stream = stream
  *
  *  - auth: (default: null) `{type:'oauth',token:'<my oauth token>'}` or `{type:'basic',username:'my user',password:'my password'}`
  *  - timeout: (default: 2 minutes) timeout in ms or string parsed by `ms` like `'30 minutes'`
- *  - protocol: (default: `https`) can be `http` or `https`
- *  - host: (default: `api.github.com`) can be `api.github.com`, `github.com` or `gist.github.com`
  *  - headers: (default: `{}`) override default headers in the request
  *
  * Result:
